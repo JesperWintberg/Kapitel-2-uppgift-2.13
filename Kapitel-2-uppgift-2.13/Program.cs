bb00d7bfced57a220fe3af377f3a559d86c2ac3c
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-09-07 18:33:51 +0200

    Add project files.

---
+++ Kapitel-2-uppgift-2.13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace uppgift_2_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in lön 1");
+            string lön1 = Console.ReadLine();
+            Console.WriteLine("Matan in lön 2");
+            string lön2 = Console.ReadLine();
+            Console.WriteLine("Mata in lön 3");
+            string lön3 = Console.ReadLine();
+
+            float lön1_tal = float.Parse(lön1);
+            float lön2_tal = float.Parse(lön2);
+            float lön3_tal = float.Parse(lön3);
+
+            float Samman = lön1_tal + lön2_tal + lön3_tal; 
+
+            Console.WriteLine("Sammanlagda lönenen är" + Samman);   
+
+        }
+    }
+}
